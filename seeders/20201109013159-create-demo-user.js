291ed96d5f8164de22a4cf40db66e4692eec9782
FacuBar <efacubar@gmail.com>
2022-01-21 00:18:40 -0300

    couldnt make use of hooks, inserting hashed password

--- seeders/20201109013159-create-demo-user.js
+++ seeders/20201109013159-create-demo-user.js
@@ -2,17 +2,27 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Users', [{
-      firstName: 'Usuario',
-      lastName: 'Demo',
-      email: 'test@test.com',
-      // Important: Password not encrypted yet! 
-      password: '1234',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    }], {});
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          firstName: 'Usuario',
+          lastName: 'Demo',
+          email: 'test@test.com',
+          // password: '1234'
+          password:
+            '$2a$10$F8Pw/Kjji4.QSbP/HN101.9N4SPy/xtr2CkQPbe7Jq1N90tBDAhd2',
+          roleId: 1,
+          image:
+            'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ]
+      // {
+      //   individualHooks: true,
+      // }
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -22,5 +32,5 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
