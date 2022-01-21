f229318bee6c60291d797c93f071e3512c8adac1
FacuBar <efacubar@gmail.com>
2022-01-21 01:25:11 -0300

    applying validators

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 const userController = require('../controllers/user');
+const userValidator = require('../middleware/userValidation');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/register', userController.register);
-router.post('/login', userController.login);
+router.post('/register', userValidator.register, userController.register);
+router.post('/login', userValidator.login, userController.login);
 
 module.exports = router;
