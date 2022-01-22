968d510f14cddcfbf2688124422a813a45a1009f
thadek <gabipamic@gmail.com>
2022-01-22 05:08:05 -0300

    Controller added

---
+++ controllers/CategoryController.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    createCategory: (req, res) => {
+
+    },
+
+    getCategories: (req, res) => {
+       res.json({msg:"controller working"})
+    },
+
+    getCategory: (req, res) => {
+
+    },
+
+    updateCategory: (req, res) => {
+
+    },
+
+    deleteCategory: (req, res) => {
+
+    }
+
+}
