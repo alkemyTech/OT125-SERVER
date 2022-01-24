f56388c8a7e991871b17e24e56e97641033b8b4a
Juan Gonzalez <juanco7133@gmail.com>
2022-01-24 20:18:48 -0300

    organization controllers added

---
+++ controllers/orgController.js
@@ -0,0 +1,20 @@
+const Organization = require('../models/organization');
+
+
+exports.orgGet = function(req,res) {
+    res.send('NOT IMPLEMENTED: Get all the organization')
+};
+
+exports.orgCreate = function(req,res) {
+    res.send('NOT IMPLEMENTED: Organization create')
+};
+
+exports.orgUpdate = function(req,res) {
+    res.send('NOT IMPLEMENTED: Organization update')
+};
+
+exports.orgDelete = function(req,res) {
+    res.send('NOT IMPLEMENTED: Organization delete')
+};
+
+
