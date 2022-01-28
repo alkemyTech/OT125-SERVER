3d9ae7bd648f2015b8b9aecd40b74ba71135d137
DarioSilva7 <darioemmanuelsilva@gmail.com>
2022-01-28 18:26:20 -0300

    send email when registering user

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ var router = express.Router();
 /* GET home page. */
 
 const roleRouter = require('./role')
+const userRouter = require('./users')
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.use('/user', userRouter )
 router.use('/role', roleRouter )
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const authRouter = require('./auth')
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+router.use('/auth', authRouter);
 
 module.exports = router;
