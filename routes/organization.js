b4245f468a18d503d60b4256dd97719f613e2da6
FacuBar <efacubar@gmail.com>
2022-01-29 11:03:06 -0300

    fixing breaking code

--- routes/organization.js
+++ routes/organization.js
@@ -2,16 +2,14 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/orgController');
 
-
 router.get('/', controller.orgGet);
 
 router.get('/:id', controller.orgGetOne);
 
 router.post('/', controller.orgUpdate);
 
-router.detele('/:id', controller.orgDelete);
+router.delete('/:id', controller.orgDelete);
 
 router.patch('/:id', controller.orgUpdate);
-
 
 module.exports = router;
