3fb9cece85e71bbe19fd26efb1d8d9146ac6b21f
Matias <mts2311@hotmail.com>
2022-02-01 18:35:58 -0300

    fixed model and migration new

--- migrations/new.js
+++ migrations/new.js
@@ -31,6 +31,14 @@ module.exports = {
             },
             deletedAt: {
                 type: Sequelize.DATE
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
             }
         })
     },
