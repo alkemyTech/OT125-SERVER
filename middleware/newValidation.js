7a6f4799c4647a6f19c95aa8600af846c7ea7ee7
Matias <mts2311@hotmail.com>
2022-02-01 18:50:33 -0300

    added new validations

---
+++ middleware/newValidation.js
@@ -0,0 +1,51 @@
+const { body, param, validationResult} = require('express-validator')
+const db = require('../models')
+
+const name = body('name')
+  .exists().withMessage('Param required')
+  .notEmpty().withMessage('Not empty')
+  .isString().withMessage('Must be a string')
+
+const content = body('content')
+  .exists().withMessage('Param required')
+  .notEmpty().withMessage('Not empty')
+  .isString().withMessage('Must be a string')
+
+const image = body('image')
+  .exists().withMessage('Param required')
+  .notEmpty().withMessage('Not empty')
+  .isString().withMessage('Must be a string')
+
+const idParam = param('id')
+  .exists().withMessage('Param required')
+  .isInt().withMessage('Must be a integer')
+  .custom(function(value){
+    return db.New.findOne({ where:{ id : value }})
+        .then(result => {
+            if(!result){
+              return Promise.reject(`Nonexistent id: ${value}`)
+            }
+        })
+  })
+
+const categoryId = body('categoryId')
+  .custom(value => {
+    if ((+value && value > 0) || !value) {
+      return true
+    }
+    throw new Error('Must be greater than 0')
+  })
+
+  const validation = (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    next()
+  }
+
+module.exports = {
+  validatorCreate: [name, content, image, categoryId, validation],
+  validatorRemove: [idParam, validation],
+  validatorGetOne: [idParam, validation],
+}
