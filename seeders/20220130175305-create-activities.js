32d15354fe34bfa025652346b1c102e434bf2c4b
FacuBar <efacubar@gmail.com>
2022-02-01 22:21:55 -0300

    adding dates on bulk insert

--- seeders/20220130175305-create-activities.js
+++ seeders/20220130175305-create-activities.js
@@ -2,27 +2,39 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-
-    await queryInterface.bulkInsert('activities', [
-      {
-      name: 'Actividad Test',
-      content: 'Demo',
-      image:'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg'
-    },{
-      name: 'Actividad Test2',
-      content: 'Demo2',
-      image:'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg'
-    },{
-      name: 'Actividad Test3',
-      content: 'Demo3',
-      image:'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg'
-    }
-  ], {});
-
+    await queryInterface.bulkInsert(
+      'activities',
+      [
+        {
+          name: 'Actividad Test',
+          content: 'Demo',
+          image:
+            'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Actividad Test2',
+          content: 'Demo2',
+          image:
+            'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Actividad Test3',
+          content: 'Demo3',
+          image:
+            'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('activities', null, {});
-
-  }
+  },
 };
