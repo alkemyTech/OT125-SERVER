3930ab8c92ef2e5c30f2828504464aebc7f03ba6
FacuBar <efacubar@gmail.com>
2022-02-03 11:39:16 -0300

    adding authent and authoriz to endpoint

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const userController = require('../controllers/user');
+const { isAdmin } = require('../middleware/isAdmin');
+const { authenticate } = require('../middleware/authenticate');
 
 /* GET users listing. */
-router.get('/', userController.getAll);
+router.get('/', authenticate, isAdmin, userController.getAll);
 
 router.delete('/:id', userController.delete);
 
