8b961b1a568cacd22b757694b827b16d019cdc91
FacuBar <efacubar@gmail.com>
2022-02-03 11:53:52 -0300

    default export on middlewares

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/CategoryController')
-const { createValidator, getOneValidator } = require('../middleware/categoryValidation')
-const { isAdmin } = require('../middleware/isAdmin')
-const { authenticate: auth } = require('../middleware/authenticate')
+const controller = require('../controllers/CategoryController');
+const {
+  createValidator,
+  getOneValidator,
+} = require('../middleware/categoryValidation');
+const isAdmin = require('../middleware/isAdmin');
+const auth = require('../middleware/authenticate');
 
 /* GET categories */
 router.get('/', auth, isAdmin, controller.getCategories);
@@ -17,6 +20,5 @@ router.patch('/:id', auth, isAdmin, controller.updateCategory);
 
 /* DELETE categories */
 router.delete('/:id', auth, isAdmin, controller.deleteCategory);
-
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const userController = require('../controllers/user');
-const { isAdmin } = require('../middleware/isAdmin');
-const { authenticate } = require('../middleware/authenticate');
+const isAdmin = require('../middleware/isAdmin');
+const auth = require('../middleware/authenticate');
 
 /* GET users listing. */
-router.get('/', authenticate, isAdmin, userController.getAll);
+router.get('/', auth, isAdmin, userController.getAll);
 
 router.delete('/:id', userController.delete);
 
