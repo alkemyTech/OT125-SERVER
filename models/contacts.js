c6712a66e84f69a93e9b49e78a0f03880921d797
thadek <gabipamic@gmail.com>
2022-02-02 23:51:34 -0300

    minor fix

--- models/contacts.js
+++ models/contacts.js
@@ -1,10 +1,8 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Contacts extends Model {
- 
+
     static associate(models) {
       // define association here
     }
