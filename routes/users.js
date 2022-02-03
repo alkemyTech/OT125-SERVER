f43384ae992b7c14d60d7dce5f561648a2160426
DarioSilva7 <darioemmanuelsilva@gmail.com>
2022-02-02 22:55:43 -0300

    Reparando error en views

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const authRouter = require('./auth')
+
+const userController = require('../controllers/user');
 
 /* GET users listing. */
-router.use('/auth', authRouter);
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.delete('/:id', userController.delete )
 
 module.exports = router;
