7697fa539bb756e0a80fb38d699d621f655a0db3
RominaSaravia <61885187+RominaSaravia@users.noreply.github.com>
2022-02-07 18:07:19 -0300

    Update membersValidator.js

--- middleware/membersValidator.js
+++ middleware/membersValidator.js
@@ -6,26 +6,14 @@ module.exports = {
     body('name').notEmpty().withMessage(`name is required`)
       .isString().withMessage('Field name only accepts strings'),
     body('image').isString().withMessage('Invalidate url, must be string'),
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      } else {
-        next()
-      }
-    }
+    errHandler
+
   ]
   ,
   VgetOne: [
     param('id').isInt().withMessage('Invalid id value. Only integers.')
       .notEmpty().withMessage('Id is required param.'),
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      } else {
-        next()
-      }
-    }
+    errHandler
+
   ]
 }
