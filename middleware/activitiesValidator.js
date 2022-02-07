e954b94b94a46a6b01e56588231dc6c604938072
RominaSaravia <61885187+RominaSaravia@users.noreply.github.com>
2022-02-07 18:06:33 -0300

    Update activitiesValidator.js

--- middleware/activitiesValidator.js
+++ middleware/activitiesValidator.js
@@ -5,23 +5,11 @@ module.exports.create = [
   body('name').notEmpty().withMessage(`name is required`).isString().withMessage(`must be string`),
   body('content').notEmpty().withMessage(`content is required`).isString().withMessage(`must be string`),
   body('image').notEmpty().withMessage(`image is required`).isString().withMessage(`must be string`),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    } else {
-      next()
-    }
-  }
+  errHandler
+
 ];
 module.exports.validateId = [
   param('id').notEmpty().withMessage(`id is required`).isNumeric().withMessage(`must be integer`),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    } else {
-      next()
-    }
-  }
+  errHandler
+
 ]
