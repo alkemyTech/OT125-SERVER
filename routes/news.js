17d857230eb7312caece38842713d45273927524
Matías Lopez <66541141+sebastos23@users.noreply.github.com>
2022-02-07 23:05:25 -0300

    Ot 125 46 47 50 news (#22)
    * fixed model and migration new
    
    * added endpoints news
    
    * added new validations
    
    Co-authored-by: FacuBar <efacubar@gmail.com>

--- routes/news.js
+++ routes/news.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
+<<<<<<< HEAD
+const {validatorCreate, validatorGetOne, validatorRemove} = require('../middleware/newValidation')
+const { getAll, getOne, create, update, remove} = require('../controllers/newsController')
+
+router.get('/', getAll);
+router.get('/:id', validatorGetOne, getOne);
+router.post('/', validatorCreate, create);
+router.patch('/:id', update);
+router.delete('/:id', validatorRemove , remove);
+=======
 const {validatorCreate, validatorGetOne, validatorUpdate, validatorRemove} = require('../middleware/newValidation');
 const { getAll, getOne, create, update, remove} = require('../controllers/newsController');
 const isAdmin = require('../middleware/isAdmin')
@@ -10,5 +20,6 @@ router.get('/:id', authenticate, isAdmin, validatorGetOne, getOne);
 router.post('/',  authenticate, isAdmin, validatorCreate, create);
 router.patch('/:id', authenticate, isAdmin, validatorUpdate, update);
 router.delete('/:id', authenticate, isAdmin, validatorRemove , remove);
+>>>>>>> develop
 
 module.exports = router;
