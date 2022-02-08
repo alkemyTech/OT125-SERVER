42ceadf52128b5c63b96e286d7ae8b18fa17520c
Matias <mts2311@hotmail.com>
2022-02-07 22:32:28 -0300

    resolve fix en newRoute

--- routes/news.js
+++ routes/news.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {validatorCreate, validatorGetOne, validatorUpdate, validatorRemove} = require('../middleware/newValidation');
+const {validatorCreate, validatorGetOne, validatorUpdate, validatorRemove, getAllValidator} = require('../middleware/newValidation');
 const { getAll, getOne, create, update, remove} = require('../controllers/newsController');
 const { isAdmin } = require('../middleware/isAdmin');
-const { authenticate } = require('../middleware/authenticate')
+const { authenticate } = require('../middleware/authenticate');
 
-router.get('/', getAll);
-router.get('/:id',/*  authenticate, isAdmin, */ validatorGetOne, getOne);
-router.post('/',/*  authenticate, isAdmin, */ validatorCreate, create);
-router.patch('/:id',/*  authenticate, isAdmin, */ validatorUpdate, update);
-router.delete('/:id',/*  authenticate, isAdmin, */ validatorRemove , remove);
+router.get('/', authenticate, getAllValidator, getAll);
+router.get('/:id', authenticate, isAdmin, validatorGetOne, getOne);
+router.post('/', authenticate, isAdmin, validatorCreate, create);
+router.patch('/:id', authenticate, isAdmin, validatorUpdate, update);
+router.delete('/:id', authenticate, isAdmin, validatorRemove , remove);
 
 module.exports = router;
