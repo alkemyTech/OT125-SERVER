9a96fdd985a0840a91775cfbdf6592670cfc3ad2
FacuBar <efacubar@gmail.com>
2022-02-08 00:28:37 -0300

    fixing controllers

--- controllers/members.js
+++ controllers/members.js
@@ -1,42 +1,40 @@
 const repository = require('../repositories/members');
 const asyncWrapper = require('../utils/asyncWrapper');
 
-let membersControllers={
-  
-      create:asyncWrapper(async (req, res, next) => {
-        const body = { name: req.body.name, image: req.body.image }
+let membersControllers = {
+  create: asyncWrapper(async (req, res, next) => {
+    const body = { name: req.body.name, image: req.body.image };
 
-        await repository.create(body, cb => {
-          if (cb.message) {
-            res
-            .status(errJSON.statusCode)
-            .json({ errors: [{ msg: errJSON.message }] });
-          } else {
-            res.json(cb)
-          }
-        })
+    await repository.create(body, (cb) => {
+      if (cb.message) {
+        res
+          .status(errJSON.statusCode)
+          .json({ errors: [{ msg: errJSON.message }] });
+      } else {
+        res.json(cb);
+      }
+    });
+  }),
 
-      });
-  
-      //Get all activities
-      findAll:asyncWrapper(async (req, res) => {
-        repository.getAll()
-          .then(result =>res.json(result))
-          .catch(err=>res.status(500).json(err));
+  //Get all activities
+  findAll: asyncWrapper(async (req, res) => {
+    repository
+      .getAll()
+      .then((result) => res.json(result))
+      .catch((err) => res.status(500).json(err));
+  }),
 
-      });
-  
-      edit:function(req,res,next){
-        res.send('Members get  editions');
-      },
-  
-      update:function(req,res,next){
-        res.send('Members update');
-      },
-  
-      destroy:function(req,res,next){
-        res.send('Members deleted');
-      },
+  edit: function (req, res, next) {
+    res.send('Members get  editions');
+  },
+
+  update: function (req, res, next) {
+    res.send('Members update');
+  },
+
+  destroy: function (req, res, next) {
+    res.send('Members deleted');
+  },
 };
 
 module.exports = membersControllers;
