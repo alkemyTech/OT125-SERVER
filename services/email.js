db16855ba5ca517b33cde6ac4ad7e29c4f72b2af
DarioSilva7 <darioemmanuelsilva@gmail.com>
2022-02-08 23:27:01 -0300

    send email synchronously

--- services/email.js
+++ services/email.js
@@ -19,9 +19,6 @@ module.exports.welcomeEmail = async (user) =>{
         console.log('Email sent successfully')
     } 
     catch(e){
-        errJSON = handleError(e);
-        return res
-        .status(errJSON.statusCode)
-        .json({ errors: [{ msg: errJSON.errMessage }] });
+        console.log(e.message);
     }
 } 
