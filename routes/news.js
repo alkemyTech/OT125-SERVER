dc4b7b6e80758b15aef50b41a7c59d9ff33eea8b
thadek <gabipamic@gmail.com>
2022-02-09 01:33:53 -0300

    minor fix on news controller

--- routes/news.js
+++ routes/news.js
@@ -19,7 +19,7 @@ const authenticate = require('../middleware/authenticate');
 router.get('/', getAll);
 router.get('/:id', authenticate, isAdmin, validatorGetOne, getOne);
 router.post('/', authenticate, isAdmin, validatorCreate, create);
-router.patch('/:id', authenticate, isAdmin, validatorUpdate, update);
+router.put('/:id', authenticate, isAdmin, validatorUpdate, update);
 router.delete('/:id', authenticate, isAdmin, validatorRemove, remove);
 
 module.exports = router;
