114f93ee93af6d349a63423636bd97f8bd557a23
FacuBar <efacubar@gmail.com>
2022-02-10 17:37:11 -0300

    create, update and delete endpoints

--- routes/comment.js
+++ routes/comment.js
@@ -6,6 +6,10 @@ const commentController = require('../controllers/comment');
 const isAdmin = require('../middleware/isAdmin');
 const auth = require('../middleware/authenticate');
 
-router.get('/', auth, isAdmin, commentController.getAll);
+// @todo implement middleware for owener || admin and add
+router.post('/', commentController.create);
+router.get('/', commentController.getAll);
+router.put('/:id', commentController.updateOne);
+router.delete('/:id', commentController.delete);
 
 module.exports = router;
