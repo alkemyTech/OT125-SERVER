cec5f872e2af8a99384a99bc8c091ca8c2d05750
Heredu <heredu_romero@hotmail.com>
2022-02-11 18:48:48 -0300

    slide-validation,repositories,controller,routes

--- controllers/slidesController.js
+++ controllers/slidesController.js
@@ -1,10 +1,23 @@
-const db = require('../models/slide');
+const repo = require('../repositories/slide');
 
 let slidesController={
 
     create:function(req, res, next) {
-        res.send('Slides')
+        res.send('Slidescreate, not implemented yet')
+      },
+    findAll:function(req, res, next) {
+        res.send('SlidesfindAll, not implemented yet')
+      },
+    findId:function(req, res, next) {
+      repo.findId();
+      },
+    update:function(req, res, next) {
+        res.send('Slidesupdate, not implemented yet')
+      },
+    destroy:function(req, res, next) {
+        res.send('Slidesdestroy, not implemented yet')
       }
+    
  
 };
 
---
+++ repositories/slide.js
@@ -0,0 +1,24 @@
+const db = require('../models/slide')
+const { Slide } = require('../models/index')
+const { handleError: errP } = require('../utils/errorHandler')
+const responseParser = require('../utils/responseFormatter')
+
+exports.create = (req, res) => {
+
+};
+
+exports.findAll = (req, res) => {
+
+};
+
+exports.findId = (req, res) => {
+    res.send('SlidesfindId, not implemented yet')
+};
+
+exports.update = (req, res) => {
+
+};
+
+exports.destroy = (req, res) => {
+
+};
