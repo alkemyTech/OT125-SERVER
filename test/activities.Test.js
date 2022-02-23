dd07a65e332c698dffa561bc1d8f4079c2479a52
FacuBar <efacubar@gmail.com>
2022-02-23 12:08:21 -0300

    minor hotfixes

--- test/activities.Test.js
+++ test/activities.Test.js
@@ -1,55 +1,53 @@
 const request = require('supertest');
 const app = require('../app');
 const chai = require('chai');
-const expect = require('chai').expect
-const chaiHttp = require('chai-http')
-chai.use(chaiHttp)
+const expect = require('chai').expect;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 
-const agent = request.agent(app)
-const admCredentials = { email: "test1@test.com", password: '1234' }
+const agent = request.agent(app);
+const admCredentials = { email: 'test1@test.com', password: '1234' };
 
-describe("ACTIVITIES - without Auth integration tests", () => {
-
+describe('ACTIVITIES - without Auth integration tests', () => {
   it('GET activities/ - without auth: should get 401', (done) => {
-      agent.get('/activities').end((err, res) => {
-          expect(res).to.have.status(401)
-          done();
-      })
-  })
-
+    agent.get('/activities').end((err, res) => {
+      expect(res).to.have.status(401);
+      done();
+    });
+  });
 
   it('#POST activities/ - without auth: should get 401 ', (done) => {
-      agent.post('/activities').end((err, res) => {
-          expect(res).to.have.status(401)
-          done();
-      })
-
-  })
-
-})
+    agent.post('/activities').end((err, res) => {
+      expect(res).to.have.status(401);
+      done();
+    });
+  });
+});
 
 describe('ACTIVITIES - testing with auth', () => {
   let jwtToken;
 
   before((done) => {
-   agent.post('/auth/login').send(admCredentials).end((err,res)=>{
-       if(err) done(err);
-       jwtToken = res.body.token;
-       done()
-   })
-  })
-
+    agent
+      .post('/auth/login')
+      .send(admCredentials)
+      .end((err, res) => {
+        if (err) done(err);
+        jwtToken = res.body.token;
+        done();
+      });
+  });
 
   it('#POST activities with auth, but empty fields: should get 400 and errors array', () => {
-       agent.post('/activities')
-       .set('Authorization', 'Bearer '+jwtToken)
-       .end((err,res)=>{
-            expect(res.body).to.have.a.property('errors')
-            expect(res).to.have.status(400)
-       })
-  })
-
-})
+    agent
+      .post('/activities')
+      .set('Authorization', 'Bearer ' + jwtToken)
+      .end((err, res) => {
+        expect(res.body).to.have.a.property('errors');
+        expect(res).to.have.status(400);
+      });
+  });
+});
 
 describe('#POST /activities', async function () {
   let jwtToken;
@@ -57,34 +55,35 @@ describe('#POST /activities', async function () {
     body: {
       name: 'Actividad Test66666',
       content: 'Demo',
-      image: 'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg'
+      image: 'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg',
     },
     expected: {
       id: 33,
       name: 'Actividad Test33',
       content: 'Demo',
-      image: 'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg'
+      image: 'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg',
     },
-    expectedStatus: 201
+    expectedStatus: 201,
   };
 
   before((done) => {
-   agent.post('/auth/login').send(admCredentials).end((err,res)=>{
-       if(err) done(err);
-       jwtToken = res.body.token;
-       done()
-   })
-  })
-
-
-  it('should create an activity successfully', async function () {
-    const res = await request(app)
-      .post('/activities')
-      .set('Authorization', 'Bearer '+jwtToken)
-      .send(test.body)
-      expect(test.expectedStatus)
+    agent
+      .post('/auth/login')
+      .send(admCredentials)
+      .end((err, res) => {
+        if (err) done(err);
+        jwtToken = res.body.token;
+        done();
+      });
   });
 
+  it('should create an activity successfully', async function () {
+    await request(app)
+      .post('/activities')
+      .set('Authorization', 'Bearer ' + jwtToken)
+      .send(test.body)
+      .expect(test.expectedStatus);
+  });
 });
 
 describe('#PUT /activities/:id', async function () {
@@ -93,50 +92,51 @@ describe('#PUT /activities/:id', async function () {
     body: {
       name: 'Actividad Test',
       content: 'Demo',
-      image: 'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg'
+      image: 'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg',
     },
     expected: {
       id: 1,
       name: 'Actividad Test',
       content: 'Demo',
-      image: 'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg'
+      image: 'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg',
     },
     _id: 1,
-    expectedStatus: 200
+    expectedStatus: 200,
   };
   const testError = {
     body: {
       name: 'Actividad Test',
       content: 'Demo',
-      image: 'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg'
+      image: 'https://live.staticflickr.com/65535/49737580966_d7eb77f786_b.jpg',
     },
     expected: {
       errors: [
         {
-          "msg": "Member test123456 with undefined undefined doesn't exists."
-        }
-      ]
+          msg: "Member test123456 with undefined undefined doesn't exists.",
+        },
+      ],
     },
     _id: 33,
-    expectedStatus: 404
+    expectedStatus: 404,
   };
 
   before((done) => {
-   agent.post('/auth/login').send(admCredentials).end((err,res)=>{
-       if(err) done(err);
-       jwtToken = res.body.token;
-       done()
-   })
-  })
-
+    agent
+      .post('/auth/login')
+      .send(admCredentials)
+      .end((err, res) => {
+        if (err) done(err);
+        jwtToken = res.body.token;
+        done();
+      });
+  });
 
   it('should update an activity successfully', async function () {
     const res = await request(app)
       .put(`/activities/${test._id}`)
-      .set('Authorization', 'Bearer '+jwtToken)
-      .send(test.body)
-      expect(test.expectedStatus)
-
+      .set('Authorization', 'Bearer ' + jwtToken)
+      .send(test.body);
+    expect(test.expectedStatus);
   });
 
   //Testing Error handler
@@ -144,9 +144,8 @@ describe('#PUT /activities/:id', async function () {
   it('member doesnt exist', async function () {
     const res = await request(app)
       .put(`/activities/${testError._id}`)
-      .set('Authorization', 'Bearer '+jwtToken)
-      .send(testError.body)
-      expect(testError.expectedStatus)
-
+      .set('Authorization', 'Bearer ' + jwtToken)
+      .send(testError.body);
+    expect(testError.expectedStatus);
   });
 });
